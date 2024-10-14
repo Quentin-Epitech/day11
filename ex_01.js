b91ec54939e1a8e83027912e427de89d6d1093a5
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-14 10:08:14 +0200

    exo1

---
+++ ex_01.js
@@ -0,0 +1,7 @@
+function promiseMe(promise) {
+    return new Promise((resolve) => {
+      resolve("I promise you to " + promise);});}
+  
+  promiseMe("fly to the moon").then((value) => {
+    console.log(value);});
+    
